9751f4e22c2cff6bec9374f59c5389ef3bba5d97
yetimoreno777 <124410746+yetimoreno777@users.noreply.github.com>
2023-06-15 19:03:40 -0700

    Practica 7 15/06/2023

---
+++ practica7.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 7</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <style>
+        .casilla {
+            height: 80px;
+            font-weight: bold;
+        }
+        .escalera{
+            background-image: url('escalera.png');
+            background-size: cover;
+        }
+        .serpiente{
+            background-image: url('serpiente.png');
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+        $colores = ["#0392cf", "#7bc043", "	#fdf498", "#f37736", "#ee4035", "#ffffff", "#bfbfbf"];
+    ?>
+    <div class="container">
+        <h1>Práctica 7 - Tablero de serpientes y escaleras</h1><hr>
+        <form action="">
+            <input type="submit" value="Jugar" class="btn btn-primary">
+            <div class="row">
+                    <?php
+                        for($i=100; $i>0; $i--){
+                            if($i == 4 || $i == 9 || $i == 33 || $i == 28 || $i == 40 || $i == 80 ||$i == 71 ||$i == 77){
+                                echo "<div class='col-1 card m-1 casilla escalera' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                            } else if($i == 17 || $i == 54 || $i == 58 || $i == 64 || $i == 87 || $i == 93 ||$i == 95 ||$i == 99 ||$i == 36){
+                                echo "<div class='col-1 card m-1 casilla serpiente' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                            } else {
+                                echo "<div class='col-1 card m-1 casilla' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                            }
+                        }
+                    ?>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
